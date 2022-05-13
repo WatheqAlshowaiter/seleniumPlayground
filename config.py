bcc292021225604fc32043dac95db57bf2ae5991
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2022-05-13 23:06:53 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+base_url = "https://ye.opensooq.com/ar"
+gecko_driver = 'geckodriver'
+chrome_driver = 'chromedriver'
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'
+page_load_timeout = 1
+
+mongo_url=<YOUR MONGODB URL>
+mongo_username=<YOUR USERNAME>
+mongo_password=<YOUR PASSWORD>
